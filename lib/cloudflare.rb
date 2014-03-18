97cd2ca21b13c0a89ad333bb87f2d48612e9de72
Greg Retkowski <gretkowski@elance.com>
2014-03-18 11:53:28 -0700

    Fix CloudFlare.connection to pass in email address
    CloudFlare.connection would nil out the email address, even if
    it was passed in. This commit fixes that behavior.

--- lib/cloudflare.rb
+++ lib/cloudflare.rb
@@ -22,6 +22,6 @@ require 'cloudflare/connection'
 
 module CloudFlare
 	def self.connection(api_key, email = nil)
-		Connection.new(api_key, email = nil)
+		Connection.new(api_key, email)
 	end
 end
