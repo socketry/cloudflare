f2e0ab99235aa66d5792e55aee6d33f723fe91d1
Rob Widmer <robert.widmer@finalsite.com>
2019-03-20 09:23:51 -0400

    Converted files to use tabs since that appears to be what is used in other files

--- lib/cloudflare/custom_hostname/ssl_attribute.rb
+++ lib/cloudflare/custom_hostname/ssl_attribute.rb
@@ -4,65 +4,65 @@ require_relative './ssl_attribute/settings'
 
 module Cloudflare
 
-  class CustomHostname < Representation
+	class CustomHostname < Representation
 
-    class SSLAttribute
+		class SSLAttribute
 
-      def initialize(params)
-        @params = params
-      end
+			def initialize(params)
+				@params = params
+			end
 
-      def active?
-        status == 'active'
-      end
+			def active?
+				status == 'active'
+			end
 
-      def cname
-        @params[:cname]
-      end
+			def cname
+				@params[:cname]
+			end
 
-      def cname_target
-        @params[:cname_target]
-      end
+			def cname_target
+				@params[:cname_target]
+			end
 
-      def http_body
-        @params[:http_body]
-      end
+			def http_body
+				@params[:http_body]
+			end
 
-      def http_url
-        @params[:http_url]
-      end
+			def http_url
+				@params[:http_url]
+			end
 
-      def method
-        @params[:method]
-      end
+			def method
+				@params[:method]
+			end
 
-      def pending_validation?
-        status == 'pending_validation'
-      end
+			def pending_validation?
+				status == 'pending_validation'
+			end
 
-      # Wraps the settings hash if it exists or initializes the settings hash and then wraps it
-      def settings
-        @settings ||= Settings.new(@params[:settings] ||= {})
-      end
+			# Wraps the settings hash if it exists or initializes the settings hash and then wraps it
+			def settings
+				@settings ||= Settings.new(@params[:settings] ||= {})
+			end
 
-      def status
-        @params[:status]
-      end
+			def status
+				@params[:status]
+			end
 
-      def to_h
-        @params
-      end
+			def to_h
+				@params
+			end
 
-      def type
-        @params[:type]
-      end
+			def type
+				@params[:type]
+			end
 
-      def validation_errors
-        @params[:validation_errors]
-      end
+			def validation_errors
+				@params[:validation_errors]
+			end
 
-    end
+		end
 
-  end
+	end
 
 end
